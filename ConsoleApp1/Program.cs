9a870fdbe0fd109991c3393fd4b9d8bc6d353f52
Mahesh Deshmukh <maheshdeshmukh258@gmail.com>
2025-06-09 14:02:31 +0530

    Implementation Completed #Learn3
    #Learn2
    
    learn#1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine("Hello, Samir!");
 Console.WriteLine("Cherry-Pick Check"); 
 Console.WriteLine("Cherry-Pick Check 2nd attempt");
 Console.WriteLine("1st Squash Commit");
-Console.WriteLine("2nd Squash Commit");
-Console.WriteLine("3nd Squash Commit");
+Console.WriteLine("2st Squash Commit");
+Console.WriteLine("3st Squash Commit");
 Console.ReadLine();
 
